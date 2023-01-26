f5b3a0e9f38b9aa9c94e8a0611aeffea3a27b9bf
Israa <48530021+israaBR@users.noreply.github.com>
2023-01-26 17:28:51 +0200

    add components and their routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
+import { CartComponent } from './components/cart/cart.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'groups', component: GroupsComponent },
+  { path: 'reservations', component: ReservationsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    GroupsComponent,
+    ReservationsComponent,
+    CartComponent,
+    MenuItemComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
